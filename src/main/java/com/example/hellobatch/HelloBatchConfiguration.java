febb4d64db96d77506b05216426cf9e9f665ff9e
Bruno <brunoantognolli@email.com>
2021-03-18 11:21:26 -0400

    Added HelloBatchConfiguration

--- src/main/java/com/example/hellobatch/HelloBatchConfiguration.java
+++ src/main/java/com/example/hellobatch/HelloBatchConfiguration.java
@@ -3,31 +3,22 @@ package com.example.hellobatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
-@SpringBootApplication
-public class HellobatchApplication {
-
+@Configuration
+public class HelloBatchConfiguration {
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
-
-    public static void main(String[] args) {
-        SpringApplication.run(HellobatchApplication.class, args);
-    }
-
 
     @Bean
     public Job job() {
@@ -42,7 +33,7 @@ public class HellobatchApplication {
         return stepBuilderFactory.get("segundaEtapa").tasklet(new Tasklet() {
             @Override
             public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("I'm here");
+                System.out.println("I'm here!");
                 return RepeatStatus.FINISHED;
             }
         }).build();
@@ -58,7 +49,4 @@ public class HellobatchApplication {
             }
         }).build();
     }
-
-
-
 }
